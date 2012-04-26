be3144d4dafcb5e76dcb50acfcab3a08e09e43cc
Danfi <t@danfi.cn>
2012-04-26 22:11:39 +0800

    project upload

---
+++ pyblog/urls.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from django.conf.urls import patterns, include, url
+from django.conf import settings
+# Uncomment the next two lines to enable the admin:
+#from django.contrib import admin
+#admin.autodiscover()
+from django.conf.urls import patterns, url, include
+from sysadmin.feeds import LatestEntriesFeed
+from sysadmin.sitemap import articleSitemap, homeSitemap
+
+
+sitemaps = {
+    'blog': articleSitemap,
+    'home':homeSitemap
+}
+
+urlpatterns = patterns('',
+    url(r'^', include('sysadmin.urls')),
+    url(r'^', include('blog.urls')),
+    # Examples:
+    # url(r'^$', 'pyblog.views.home', name='home'),
+    # url(r'^pyblog/', include('pyblog.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+#    url(r'^admin/', include(admin.site.urls)),
+    (r'^feed/$', LatestEntriesFeed()),
+    (r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}),
+)
